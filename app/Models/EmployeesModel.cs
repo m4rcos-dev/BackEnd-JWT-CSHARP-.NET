7f9c727ea61331d2e0d705f6ae275e1811cace86
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-06 12:16:51 -0300

    :sparkles: feat: add DataAnnotations in the EmployeesModel

--- app/Models/EmployeesModel.cs
+++ app/Models/EmployeesModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace app.Models
 {
   public class EmployeesModel
@@ -10,9 +12,16 @@ namespace app.Models
       this.Role = role;
     }
 
+    [Required(ErrorMessage = "The field {0} is required")]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required")]
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required")]
     public string Sector { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required")]
     public string Role { get; set; }
   }
 }
